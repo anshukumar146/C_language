d3ae5dcc1d9e3e3bfb8427ca69f49d939629dbd8
anshukumar146 <anshukumarxyz730@gmail.com>
2025-09-27 10:08:41 +0530

    Update largest_of_number.c

--- basic_c_programs/largest_of_number.c
+++ basic_c_programs/largest_of_number.c
@@ -4,13 +4,13 @@
 int main()
 {
     int num1 , num2 , num3 ;                        //Taking input from user.
-printf("Entre num1:\n");
+printf("Entre first number:\n");
 scanf(" %d",&num1);
 
-printf("Enter num2:\n");
+printf("Enter second number:\n");
 scanf(" %d",&num2);
 
-printf("Enter num3:\n");
+printf("Enter third number:\n");
 scanf(" %d",&num3);
 
 if( num1 >= num2 && num1 >= num3)                       //Check if num1 greater number between num1,num2 and num3.
