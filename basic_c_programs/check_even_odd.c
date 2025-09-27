94bda3511350ceb9e8f68875de95909e17c0dc19
anshukumar146 <anshukumarxyz730@gmail.com>
2025-09-27 10:04:40 +0530

    Update check_even_odd.c

--- basic_c_programs/check_even_odd.c
+++ basic_c_programs/check_even_odd.c
@@ -9,7 +9,7 @@ printf("Entre a:\n");                            // taking input from user.
 scanf(" %d",&a);
 
 if( a % 2 == 0)                                    //checking if remainder of a after dividing by 2 is zero or not.
-printf("%d is an even number ",a);
+printf("%d is an even number ",a);                 //if yes its even if not its odd.            
 
 else
 {printf("%d is an odd number ",a);}
