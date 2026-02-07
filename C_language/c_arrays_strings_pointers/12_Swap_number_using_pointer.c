127bef801c463a8fa958182ba335b0f838239559
anshu kumar <anshukumarxyz730@gmail.com>
2026-02-07 16:06:59 +0530

    adding 12_Swap_number_using_pointer.c

--- C_language/c_arrays_strings_pointers/12_Swap_number_using_pointer.c
+++ C_language/c_arrays_strings_pointers/12_Swap_number_using_pointer.c
@@ -2,18 +2,18 @@
 void swap( int *m,int *n);
 int main(){
 int a, b, *p, *q ;
-printf("Enter the first number: \n");
+printf("Enter the first number: \n");                   //Input number.
 scanf("%d",&a);
 printf("Enter the second number: \n");
 scanf("%d",&b);
-p = &a;
+p = &a;                                               //store address of numbers.
 q = &b;
-swap( p, q);
+swap( p, q);                                         //Call swap function.
 printf("First number = %d \t second number = %d",a , b);
 return 0;
 }
 void swap( int *m,int *n){
-int temp = *m;
+int temp = *m;                          //swap logic.
     *m = *n;
     *n=temp;
 }
